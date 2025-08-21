80c9f4ba8d75b83a58ecb171ed58da5d46ff56e4
galleb <vikeevsv@gmail.com>
2025-08-21 15:32:11 +0300

    grey icons

--- install/desktop/theme.sh
+++ install/desktop/theme.sh
@@ -4,10 +4,10 @@ sudo xbps-install -y sassc glib-devel nwg-look
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 /tmp/WhiteSur-gtk-theme
 /tmp/WhiteSur-gtk-theme/install.sh -c dark
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git /tmp/WhiteSur-icon-theme
-/tmp/WhiteSur-icon-theme/install.sh
+/tmp/WhiteSur-icon-theme/install.sh -t grey
 
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-dark"
+gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-grey-dark"
 
 source ~/.config/hypr/scripts/init_wallpaper.sh
