28298031c12995d4eabe0ee9d72007c829a5849f
galleb <vikeevsv@gmail.com>
2025-08-22 13:08:56 +0300

    edit voidpackages.sh

--- install/apps/voidpackages.sh
+++ install/apps/voidpackages.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
+read -p "Would you like to clone void-packages repo and setup additional packages? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 
-git clone https://github.com/void-linux/void-packages.git ~/.local/pkgs/void-packages
-
-(
-  echo "Entering ~/.local/pkgs/void-packages"
-
-  cd ~/.local/pkgs/void-packages || exit 1
-
-  # 1. Prepare void repo
-  ./xbps-src binary-bootstrap
-  echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+  if [ ! -d "~/.local/pkgs/void-packages" ]; then
   
-  # 2. Copy templates to srcpkgs
-  cp ~/.local/share/omvoid/srcpkgs/* ~/.local/pkgs/void-packages/srcpkgs/ 
+  (
+    git clone https://github.com/void-linux/void-packages.git ~/.local/pkgs/void-packages
+  
+    echo "Entering ~/.local/pkgs/void-packages"
+  
+    cd ~/.local/pkgs/void-packages || exit 1
+  
+    # 1. Prepare void repo
+    ./xbps-src binary-bootstrap
+    echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+    
+    # 2. Copy templates to srcpkgs
+    cp -r ~/.local/share/omvoid/srcpkgs/* ~/.local/pkgs/void-packages/srcpkgs/ 
+  
+    # 3. Install rofi-wayland
+    ./xbps-src pkg rofi-wayland
+    sudo xbps-install -y --repository hostdir/binpkgs rofi-wayland
+  
+    # 4. Install SwayOSD
+    ./xbps-src pkg SwayOSD
+    sudo xbps-install -y --repository hostdir/binpkgs SwayOSD 
+    if [ ! -L "/var/service/swayosd-libinput-backend/" ]; then
+      sudo ln -s /etc/sv/swayosd-libinput-backend /var/service
+    fi
+  
+    echo "Leaving subshell."
+  )
+  
+  fi
 
-  # 3. Install rofi-wayland
-  ./xbps-src rofi-wayland
-  xbps-install --repository hostdir/binpkgs rofi-wayland
+fi
 
-  echo "Leaving subshell."
-)
+
