8357679679525ceda0647ad3c0e33fb5d573e86a
galleb <vikeevsv@gmail.com>
2025-08-28 12:49:37 +0300

    add sleep

--- install/development/hyprtime.sh
+++ install/development/hyprtime.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Hyprland
 echo "repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc" | sudo tee /etc/xbps.d/hyprland-void.conf
+sleep 3
 sudo xbps-install -S -y \
   hyprland hyprland-devel xdg-desktop-portal-hyprland \
   hyprland hyprwayland-scanner hyprutils hyprlang hyprlock \
