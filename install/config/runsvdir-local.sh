29f1c7a416527b5fa593124682f84e7b57ddde43
galleb <vikeevsv@gmail.com>
2025-09-27 19:18:27 +0300

    make local runsvdir default

--- install/config/runsvdir-local.sh
+++ install/config/runsvdir-local.sh
@@ -7,4 +7,4 @@ sed -i 's|^exec-once = mpdris2-rs\s*$|#exec-once = mpdris2-rs|' ~/.config/hypr/c
 
 sed -i 's|^#exec-once = runsvdir -P ~/.config/service\s*$|exec-once = runsvdir -P ~/.config/service|' ~/.config/hypr/config/autostart.conf
 
-sed -i 's|^source $OMVOID_INSTALL/config/runsvdir.sh\s*$|#source $OMVOID_INSTALL/config/runsvdir-local.sh|' ~/.local/share/omvoid/install.sh
+sed -i 's|^source $OMVOID_INSTALL/config/runsvdir-local.sh\s*$|#source $OMVOID_INSTALL/config/runsvdir-local.sh|' ~/.local/share/omvoid/install.sh
