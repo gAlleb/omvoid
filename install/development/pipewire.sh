4683cb62deed1af651f4e97b904e7d2a80064461
galleb <vikeevsv@gmail.com>
2025-10-05 21:53:59 +0300

    fix pipewire.sh

--- install/development/pipewire.sh
+++ install/development/pipewire.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo xbps-install -y pipewire wireplumber
 
-sudo mkdir -p ~/.config/pipewire/pipewire.conf.d
+mkdir -p ~/.config/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf ~/.config/pipewire/pipewire.conf.d/
 
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
+mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf ~/.config/pipewire/pipewire.conf.d/
 
 sudo xbps-install -y pulseaudio-utils pavucontrol
