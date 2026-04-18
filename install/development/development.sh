5db5cb9fc259d968ddc36a32fb751bce88f7428e
galleb <vikeevsv@gmail.com>
2026-04-18 21:38:44 +0300

    add apps

--- install/development/development.sh
+++ install/development/development.sh
@@ -13,7 +13,7 @@ sudo xbps-install -y \
   kvantum qt5ct qt6ct xtools libinput-gestures mesa-dri yq lm_sensors \
   tesseract-ocr tesseract-ocr-rus tesseract-ocr-eng tesseract-ocr-san \
   gearlever mpdris2-rs fuzzypkg yazi python3-watchdog libxkbcommon-tools \
-  bind-utils ripdrag
+  bind-utils ripdrag keepassxc gajim 
 
 # X11 batch
 sudo xbps-install -y \
