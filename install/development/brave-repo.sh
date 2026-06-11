ec0b8a490e345db388b4f8bad1cce743d8039eb3
galleb <s@omfm.ru>
2026-06-11 20:54:30 +0300

    add brave-origin-repo

---
+++ install/development/brave-repo.sh
@@ -0,0 +1,11 @@
+# Jake's repo 
+
+if [ ! -d /var/db/xbps/keys ] ; then
+    sudo mkdir -p /var/db/xbps/keys
+fi 
+
+sudo cp -R ~/.local/share/omvoid/default/repokeyes/omvoid/* /var/db/xbps/keys/
+
+echo repository=https://codeberg.org/JakeAtLinux/void-repo/raw/branch/main/x86_64 | sudo tee /etc/xbps.d/jake-void-repo.conf
+
+sudo xbps-install -Sy brave-origin-bin
