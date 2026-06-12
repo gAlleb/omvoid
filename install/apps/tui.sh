6863f4150cb7065386669bff6fbf0a93f0702de5
galleb <s@omfm.ru>
2026-06-12 11:04:53 +0300

    fix tui-install

--- install/apps/tui.sh
+++ install/apps/tui.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-ICON_DIR="$HOME/.local/share/applications/icons"
-
-omvoid-tui-install "Disk Usage" "bash -c 'dust -r; read -n 1 -s'" float "$ICON_DIR/Disk Usage.png"
+omvoid-tui-install "Disk Usage" "bash -c 'dust -r; read -n 1 -s'" float "Disk Usage.png"
