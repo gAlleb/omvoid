ecf9c48c1bc84e8440cfe2f4681b385bf1451dfb
galleb <s@omfm.ru>
2026-07-13 16:17:41 +0300

    .

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -2,6 +2,6 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
-vim.lsp.enable({'luals', 'pyright'})
+vim.lsp.enable({'lua_ls', 'pyright'})
 
 -- read :h vim.lsp.config for changing options of lsp servers 
