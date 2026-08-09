c994c4e9974d2446b31eb9e1e6dbee3dc565c19e
stefan <s@3s0.ru>
2026-08-09 22:59:19 +0300

    add mesa-vulkan-lavapipe

--- install/development/development.sh
+++ install/development/development.sh
@@ -15,7 +15,7 @@ sudo xbps-install -y \
   gearlever mpdris2-rs fuzzypkg yazi python3-watchdog libxkbcommon-tools \
   bind-utils ripdrag keepassxc gajim exfat-utils fuse-exfat bat fd \
   neomutt msmtp pass gettext isync goimapnotify pam-gnupg lynx notmuch \
-  urlview abook w3m mesa-vulkan-intel vulkan-loader
+  urlview abook w3m mesa-vulkan-intel vulkan-loader mesa-vulkan-lavapipe
 
 # X11 batch
 sudo xbps-install -y \
