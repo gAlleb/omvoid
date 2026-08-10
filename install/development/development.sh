c84a02f348a9734905be1d1a946d1168d40706a8
stefan <s@3s0.ru>
2026-08-10 08:22:10 +0300

    add qt5-wayland for keepassxc

--- install/development/development.sh
+++ install/development/development.sh
@@ -28,7 +28,7 @@ sudo xbps-install -y \
   Waybar wl-clipboard wl-clip-persist cliphist wf-recorder \
   SwayNotificationCenter satty slurp grim awww walker \
   wlogout swaybg SwayOSD crystal-dock wlr-randr wlopm swaylock \
-  swayidle
+  swayidle qt5-wayland
 
 # Extra batch 
 sudo xbps-install -y \
